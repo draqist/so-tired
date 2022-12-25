7804b168192688844222e562cc8786f1907208bd
Draq <boomjay008@gmail.com>
2022-12-25 12:46:32 +0100

    feat: the start of something

---
+++ components/UI/Navbar.tsx
@@ -0,0 +1,11 @@
+import styles from "../../styles/components/navbar.module.scss"
+const Navbar = () => {
+  const {navbar} = styles
+  return (
+    <div className={navbar}>
+
+    </div>
+  )
+}
+
+export default Navbar
